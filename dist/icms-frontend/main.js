9009eacd9b8c134cc4b69a294227436206832f3a
Jiratip.W <jiratip.w@gmail.com>
2019-01-26 17:58:14 +0700

    first build

---
+++ dist/icms-frontend/main.js
@@ -0,0 +1,195 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
+
+/***/ "./src/$$_lazy_route_resource lazy recursive":
+/*!**********************************************************!*\
+  !*** ./src/$$_lazy_route_resource lazy namespace object ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function webpackEmptyAsyncContext(req) {
+	// Here Promise.resolve().then() is used instead of new Promise() to prevent
+	// uncaught exception popping up in devtools
+	return Promise.resolve().then(function() {
+		var e = new Error("Cannot find module '" + req + "'");
+		e.code = 'MODULE_NOT_FOUND';
+		throw e;
+	});
+}
+webpackEmptyAsyncContext.keys = function() { return []; };
+webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+module.exports = webpackEmptyAsyncContext;
+webpackEmptyAsyncContext.id = "./src/$$_lazy_route_resource lazy recursive";
+
+/***/ }),
+
+/***/ "./src/app/app.component.css":
+/*!***********************************!*\
+  !*** ./src/app/app.component.css ***!
+  \***********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL2FwcC5jb21wb25lbnQuY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/app.component.html":
+/*!************************************!*\
+  !*** ./src/app/app.component.html ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<nav class=\"navbar navbar-color-on-scroll fixed-top navbar-expand-lg navbar-transparent\" color-on-scroll=\"100\"\r\n     id=\"sectionsNav\">\r\n  <div class=\"container\">\r\n    <div class=\"navbar-translate\">\r\n      <a class=\"navbar-brand\" href=\"https://demos.creative-tim.com/material-kit/index.html\">\r\n        NL-INSU </a>\r\n      <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" aria-expanded=\"false\"\r\n              aria-label=\"Toggle navigation\">\r\n        <span class=\"sr-only\">Toggle navigation</span>\r\n        <span class=\"navbar-toggler-icon\"></span>\r\n        <span class=\"navbar-toggler-icon\"></span>\r\n        <span class=\"navbar-toggler-icon\"></span>\r\n      </button>\r\n    </div>\r\n    <div class=\"collapse navbar-collapse\">\r\n      <ul class=\"navbar-nav ml-auto\">\r\n        <li class=\"nav-item\">\r\n          <a class=\"nav-link\" rel=\"tooltip\" title=\"\" data-placement=\"bottom\" href=\"https://www.facebook.com/zorzosi\"\r\n             target=\"_blank\" data-original-title=\"Like us on Facebook\">\r\n            <i class=\"fa fa-facebook-square\"></i>\r\n          </a>\r\n        </li>\r\n        <li class=\"nav-item\">\r\n          <a class=\"nav-link\" rel=\"tooltip\" title=\"\" data-placement=\"bottom\"\r\n             href=\"https://www.instagram.com/zorzoary\" target=\"_blank\"\r\n             data-original-title=\"Follow us on Instagram\">\r\n            <i class=\"fa fa-instagram\"></i>\r\n          </a>\r\n        </li>\r\n      </ul>\r\n    </div>\r\n  </div>\r\n</nav>\r\n<div class=\"page-header header-filter clear-filter purple-filter\" data-parallax=\"true\"\r\n     style=\"background-image: url(&quot;./assets/img/bg2.jpg&quot;);\">\r\n  <div class=\"container\">\r\n    <div class=\"row\">\r\n      <div class=\"col-md-8 ml-auto mr-auto\">\r\n        <div class=\"brand\">\r\n          <h1 style=\"text-align: center;\">ห้องสมุดประกันวินาศภัย</h1>\r\n          <h3 style=\"text-align: center;\">อยากรู้เกี่ยวกับประกันสามารถเรียนรู้ได้ที่นี่</h3>\r\n        </div>\r\n      </div>\r\n    </div>\r\n  </div>\r\n</div>\r\n<div class=\"main main-raised\">\r\n  <div class=\"section section-basic\">\r\n    <div class=\"container\">\r\n      <div class=\"title\">\r\n        <h2>บทความ</h2>\r\n      </div>\r\n      <div class=\"row\">\r\n        <div class=\"col-lg-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 1\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n            <div class=\"card-footer card-read-continue-link\">\r\n              <a href=\"#\">อ่านต่อ>></a>\r\n            </div>\r\n          </div>\r\n        </div>\r\n        <div class=\"col-md-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 2\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n            <div class=\"card-footer card-read-continue-link\">\r\n              <a href=\"#\">อ่านต่อ>></a>\r\n            </div>\r\n          </div>\r\n        </div>\r\n        <div class=\"col-md-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 3\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n            <div class=\"card-footer card-read-continue-link\">\r\n              <a href=\"#\">อ่านต่อ>></a>\r\n            </div>\r\n          </div>\r\n        </div>\r\n      </div>\r\n    </div>\r\n  </div>\r\n</div>\r\n\r\n\r\n<div class=\"main main-raised\" style=\"margin-top: 50px;\">\r\n  <div class=\"section section-basic\">\r\n    <div class=\"container\">\r\n      <div class=\"title\">\r\n        <h2>การคำนวน</h2>\r\n      </div>\r\n      <div class=\"row\">\r\n        <div class=\"col-lg-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 1\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n          </div>\r\n        </div>\r\n        <div class=\"col-md-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 2\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n          </div>\r\n        </div>\r\n        <div class=\"col-md-4\">\r\n          <div class=\"card\">\r\n            <div class=\"card-header card-header-info\">\r\n              ห้วข้อ 3\r\n            </div>\r\n            <div class=\"card-body\">\r\n              ประกันภัยหมายถึง “สัญญาที่จะจ่ายสินไหมทดแทนสำหรับการสูญเสียบางอย่างที่อาจเกิดขึ้นในอนาคต\r\n              แลกกับการจ่ายเงินเป็นงวดๆ ประกันภัยออกแบบมาเพื่อปกป้องความมั่นคงทางการเงินของบุคคล บริษัท หรือองค์กรใดๆ\r\n              ก็ตาม ในกรณีที่เกิดเหตุการณ์ไม่คาดฝัน”\r\n            </div>\r\n          </div>\r\n        </div>\r\n      </div>\r\n    </div>\r\n  </div>\r\n</div>\r\n"
+
+/***/ }),
+
+/***/ "./src/app/app.component.ts":
+/*!**********************************!*\
+  !*** ./src/app/app.component.ts ***!
+  \**********************************/
+/*! exports provided: AppComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppComponent", function() { return AppComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'icms-frontend';
+    }
+    AppComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-root',
+            template: __webpack_require__(/*! ./app.component.html */ "./src/app/app.component.html"),
+            styles: [__webpack_require__(/*! ./app.component.css */ "./src/app/app.component.css")]
+        })
+    ], AppComponent);
+    return AppComponent;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/app.module.ts":
+/*!*******************************!*\
+  !*** ./src/app/app.module.ts ***!
+  \*******************************/
+/*! exports provided: AppModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AppModule", function() { return AppModule; });
+/* harmony import */ var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/platform-browser */ "./node_modules/@angular/platform-browser/fesm5/platform-browser.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app.component */ "./src/app/app.component.ts");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            declarations: [
+                _app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]
+            ],
+            imports: [
+                _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"]
+            ],
+            providers: [],
+            bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/environments/environment.ts":
+/*!*****************************************!*\
+  !*** ./src/environments/environment.ts ***!
+  \*****************************************/
+/*! exports provided: environment */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+var environment = {
+    production: false
+};
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
+/***/ }),
+
+/***/ "./src/main.ts":
+/*!*********************!*\
+  !*** ./src/main.ts ***!
+  \*********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/platform-browser-dynamic */ "./node_modules/@angular/platform-browser-dynamic/fesm5/platform-browser-dynamic.js");
+/* harmony import */ var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/app.module */ "./src/app/app.module.ts");
+/* harmony import */ var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./environments/environment */ "./src/environments/environment.ts");
+
+
+
+
+if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+    Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["enableProdMode"])();
+}
+Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformBrowserDynamic"])().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])
+    .catch(function (err) { return console.error(err); });
+
+
+/***/ }),
+
+/***/ 0:
+/*!***************************!*\
+  !*** multi ./src/main.ts ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! D:\work\ZORZO\front-end\src\main.ts */"./src/main.ts");
+
+
+/***/ })
+
+},[[0,"runtime","vendor"]]]);
+//# sourceMappingURL=main.js.map
