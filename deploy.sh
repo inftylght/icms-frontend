c535a0e2cbdf085cfd2ce1af64b9111eb14a7f7a
Jiratip.W <jiratip.w@gmail.com>
2019-02-22 22:58:38 +0700

    add base directory

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
 git pull
 npm install
 ng build --prod
 rm -rf /var/www/frontend/html/*
 cp -a -f dist/icms-frontend/. /var/www/frontend/html/
+
