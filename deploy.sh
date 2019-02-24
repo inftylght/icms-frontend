3da04c73842df7841875e115ea2f7fa254bae129
Jiratip.W <jiratip.w@gmail.com>
2019-02-25 00:02:06 +0700

    add git reset for reset code before pull code

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
+git reset --hard
 git pull
 npm install
-npm audit fix
 ng build --prod
 rm -rf /var/www/frontend/html/*
 cp -a -f dist/icms-frontend/. /var/www/frontend/html/
