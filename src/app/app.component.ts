24de5d8126734483b753abeff8dfcf7473a6cc05
Jiratip.W <jiratip.w@gmail.com>
2019-02-25 22:31:07 +0700

    add title change language

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {LocalStorage} from 'ngx-webstorage';
 
 @Component({
@@ -6,13 +6,32 @@ import {LocalStorage} from 'ngx-webstorage';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor() {
+  }
 
   @LocalStorage('language')
   private langulage;
 
+  public title;
+  public titleDescription;
+
+  changeTitleByLang(language) {
+    if (language === 'EN') {
+      this.title = 'Non-life insurance library';
+      this.titleDescription = 'Curious about insurance? you can learn here.';
+    } else {
+      this.title = 'ห้องสมุดประกันวินาศภัย';
+      this.titleDescription = 'อยากรู้เกี่ยวกับประกันสามารถเรียนรู้ได้ที่นี่';
+    }
+  }
+
   onChangeLang(language) {
     this.langulage = language;
+    this.changeTitleByLang(this.langulage);
+  }
+
+  ngOnInit(): void {
+    this.changeTitleByLang(this.langulage);
   }
 }
