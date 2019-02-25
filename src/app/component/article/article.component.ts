492d6db216464cdfa9b18b2d320c48c5237a5063
Jiratip.W <jiratip.w@gmail.com>
2019-02-25 22:21:42 +0700

    fix article cannot change language

--- src/app/component/article/article.component.ts
+++ src/app/component/article/article.component.ts
@@ -1,35 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {ArticleService} from "../../service/article.service";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ArticleService} from '../../service/article.service';
+import {LocalStorage, LocalStorageService} from 'ngx-webstorage';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnDestroy {
 
   private routerSubscription;
   private articleId;
   public text;
   public title;
 
-  constructor(private route:ActivatedRoute,
-              private articleService:ArticleService) {}
+  private article;
+
+  @LocalStorage('language')
+  private language;
+
+  constructor(private route: ActivatedRoute,
+              private articleService: ArticleService,
+              private localStorageService: LocalStorageService) {
+  }
 
   ngOnInit() {
-    this.text="";
+    this.text = '';
+    this.localStorageService.observe('language')
+      .subscribe((language) => {
+        setArticleByLanguage(language);
+      });
+
+    const setArticleByLanguage = (language) => {
+      if (language === 'EN') {
+        this.title = this.article.titleEN;
+        this.text = this.article.textEN;
+      } else {
+        this.title = this.article.title;
+        this.text = this.article.text;
+      }
+    };
+
     this.routerSubscription = this.route.params.subscribe((param) => {
       this.articleId = param['id'];
       this.articleService.get(this.articleId)
         .then(data => {
-          this.title = data.title;
-          this.text = data.text;
-        })
-
-    })
+          this.article = data;
+          setArticleByLanguage(this.language);
+        });
+    });
   }
- 
+
   ngOnDestroy() {
     this.routerSubscription.unsubscribe();
   }
